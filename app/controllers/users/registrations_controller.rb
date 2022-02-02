8f57542598a8efab8217496886fa1b49b14e686e
rmuehe <rmuehe@gmail.com>
2022-02-02 04:28:55 +0200

    Update registrations controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,2 +1,23 @@
-class Users::RegistrationsController < ApplicationController
-end
+# class Users::RegistrationsController < ApplicationController
+# end
+
+# overrides the Devise controller to handle JSON responses
+class Users::RegistrationsController < Devise::RegistrationsController
+    respond_to :json
+  
+    private
+  
+    def respond_with(resource, _opts = {})
+      register_success && return if resource.persisted?
+  
+      register_failed
+    end
+  
+    def register_success
+      render json: { message: 'Signed up sucessfully.' }, status: :created
+    end
+  
+    def register_failed
+      render json: { message: "Something went wrong." }, status: :bad_request
+    end
+  end
