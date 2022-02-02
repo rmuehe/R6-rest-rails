18ba0d2e23679d6f069f02f8047bf812e0c882f6
rmuehe <rmuehe@gmail.com>
2022-02-02 03:58:20 +0200

    Install devise & generate devise User

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+# Ensure you have defined root_url to *something* in your config/routes.rb.
+#      For example:
+
+#        root to: "home#index"
+     
+#      * Not required for API-only Applications *
