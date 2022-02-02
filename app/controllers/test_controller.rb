4a732444e5088932fa3e9a624d5d3cd40411271c
rmuehe <rmuehe@gmail.com>
2022-02-02 04:42:58 +0200

    Update test controller

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,2 +1,14 @@
+# class TestController < ApplicationController
+# end
+
+# test controller to verify that login works
 class TestController < ApplicationController
-end
+    include AuthenticationCheck
+  
+    before_action :is_user_logged_in
+  
+    def show
+      render json: { message: "If you see this, you're logged in!" },
+        status: :ok
+    end
+  end
